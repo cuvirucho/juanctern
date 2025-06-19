131e66584badd0d3edaa40d946660b44880366a3
cuvirucho <dani.leonardo@hotmail.com>
2025-06-19 16:58:16 -0600

    Actualiza src/firabse.js y agrega archivo de configuración .env

--- src/firabse.js
+++ src/firabse.js
@@ -8,14 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCN_2D-qxi4sUla7jI_16u7j8IGTRTGE_Q",
-  authDomain: "caterinjuan-bf892.firebaseapp.com",
-  projectId: "caterinjuan-bf892",
-  storageBucket: "caterinjuan-bf892.firebasestorage.app",
-  messagingSenderId: "579783358770",
-  appId: "1:579783358770:web:7b031361bd34bbf7c23f2b",
-  measurementId: "G-3FPPLSX1W7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
